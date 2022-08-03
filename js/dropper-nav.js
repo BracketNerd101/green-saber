4f5138e045f4980aaa2250e11948de50f52ebfdf
Arthur C Edwards <Arthur.Edwards321@gmail.com>
2022-08-03 01:19:03 -0400

    added link for bootstrap icons

--- js/dropper-nav.js
+++ js/dropper-nav.js
@@ -2,7 +2,7 @@
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 30) {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 230) {
         document.getElementById("nav-dropper").style.top = "0";
     } else {
         document.getElementById("nav-dropper").style.top = "-150px";
